e03464ec0032ea1d949a943b08106cf82f34ddf9
johnyuh <125418044+johnyuh@users.noreply.github.com>
2025-10-18 22:33:57 +0800

    Change GitHub token in config.js
    Updated GitHub token in configuration.

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 window.OneSparkConfig = {
   GITHUB_USER: "johnyuh",
   GITHUB_REPO: "onespark-app",
-  GITHUB_TOKEN: "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" // ← 請填入你自己的 GitHub Token
+  GITHUB_TOKEN: "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" // ← 請填入你自己的 GitHub Token
 };
