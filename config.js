4ec58430dd455b376f958ae93ac739b0a4c7c5ce
johnyuh <125418044+johnyuh@users.noreply.github.com>
2025-10-19 10:44:56 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-window.OneSparkConfig = {
-  GITHUB_USER: "johnyuh",
-  GITHUB_REPO: "onespark-app",
-  GITHUB_TOKEN: "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" // ← 請填入你自己的 GitHub Token
-};
